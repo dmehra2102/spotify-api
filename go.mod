777d26a4422923d8d5c5ceff3368c8154ccdc794
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-12-10 11:49:09 +0530

    feat: added User model structure

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dmehra2102/spotify-api
 
 go 1.25.1
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+)
---
+++ internal/domain/models/user.go
@@ -0,0 +1,52 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID           string    `json:"id"`
+	Email        string    `json:"email"`
+	Name         string    `json:"name"`
+	AvatarURL    string    `json:"avatar_url"`
+	Bio          string    `json:"bio"`
+	HashPassword string    `json:"hash_password"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type UserCredentials struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type UserProfile struct {
+	ID             string    `json:"id"`
+	Email          string    `json:"email"`
+	Name           string    `json:"name"`
+	AvatarURL      string    `json:"avatar_url"`
+	Bio            string    `json:"bio"`
+	FollowersCount int       `json:"followers_count"`
+	FollowingCount int       `json:"following_count"`
+	SongsCount     int       `json:"songs_count"`
+	CreatedAt      time.Time `json:"created_at"`
+}
+
+type JWTClaims struct {
+	UserID string `json:"user_id"`
+	Email  string `json:"email"`
+}
+
+func NewUser(email, name, hashedPassword string) *User {
+	now := time.Now()
+	return &User{
+		ID:           uuid.New().String(),
+		Email:        email,
+		Name:         name,
+		HashPassword: hashedPassword,
+		CreatedAt:    now,
+		UpdatedAt:    now,
+	}
+}
